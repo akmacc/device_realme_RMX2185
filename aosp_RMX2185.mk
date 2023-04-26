1d1ef52efcaf60db8cded8b2504d6e435e0424ef
akmacc <akjagxz@outlook.com>
2023-04-27 01:40:41 +0530

    pixelE-ify

--- aosp_RMX2185.mk
+++ aosp_RMX2185.mk
@@ -17,16 +17,18 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 # Inherit from device makefile
 $(call inherit-product, device/realme/RMX2185/device.mk)
 
 # Inherit some common stuff.
+TARGET_FACE_UNLOCK_SUPPORTED := true
 TARGET_BOOT_ANIMATION_RES := 720
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+TARGET_GAPPS_ARCH := arm64
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_RMX2185
+PRODUCT_NAME := aosp_RMX2185
 PRODUCT_DEVICE := RMX2185
 PRODUCT_BRAND := realme
 PRODUCT_MODEL := Realme G35 Series
@@ -44,5 +46,3 @@ PRODUCT_GMS_CLIENTID_BASE := android-oppo
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.build.fingerprint=$(BUILD_FINGERPRINT)
 
-# Time
-LINEAGE_VERSION_APPEND_TIME_OF_DAY := true
